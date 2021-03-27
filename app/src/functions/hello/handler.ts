af8046501c72c92de6942f1aca967434f88a7098
TakashiKominami <dev.kominami@gmail.com>
2021-03-27 21:26:27 +0900

    feat(*): prettierフォーマット設定

--- app/src/functions/hello/handler.ts
+++ app/src/functions/hello/handler.ts
@@ -1,17 +1,17 @@
-import 'source-map-support/register'
+import 'source-map-support/register';
 
 // import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 // import { formatJSONResponse } from '@libs/apiGateway';
-import { middyfy } from '@libs/lambda'
+import { middyfy } from '@libs/lambda';
 
 // import schema from './schema';
 
 const hello = async (event) => {
-    return `Hello ${event.body.name}, welcome to the exciting Serverless world!`
-    // return formatJSONResponse({
-    //   message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    //   event,
-    // });
-}
+  return `Hello ${event.body.name}, welcome to the exciting Serverless world!`;
+  // return formatJSONResponse({
+  //   message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+  //   event,
+  // });
+};
 
-export const main = middyfy(hello)
+export const main = middyfy(hello);
